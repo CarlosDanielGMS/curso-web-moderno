7b199ef69e1405954333125e7a94d20d46829045
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-01-12 11:18:14 -0300

    feat: implementa diferentes formas de comentários

---
+++ 01-fundamentos/03-comentario.js
@@ -0,0 +1,20 @@
+// A ordem das sentenças importa, pois é ela que será usada pelo dispositivo durante a execução
+
+// Primeira sentença, exibida com o comentário logo acima do código
+console.log("Passo 03");
+
+console.log("Passo 01"); // Segunda sentença, exibida com o comentário na mesma linha
+
+// console.log("Passo 02"); Terceita sentença, não exibida porque está comentada
+
+/*
+    Comentário com
+    múltiplas linhas.
+*/
+
+/*
+ * Outra forma
+ * de comentar
+ * com múltiplas
+ * linhas.
+ */
