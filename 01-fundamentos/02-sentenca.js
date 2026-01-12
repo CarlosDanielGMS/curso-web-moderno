3baf3c117a3287485a4e1e6295cbca26c1a3aa99
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-01-12 11:02:51 -0300

    feat: testando as diferentes formas de exibir informações no terminal

---
+++ 01-fundamentos/02-sentenca.js
@@ -0,0 +1,11 @@
+// Primeira sentença, exibindo um número
+console.log(7);
+
+// Segunda sentença, exibindo uma soma
+console.log(5 + 5);
+
+// Terceira sentença, exibindo uma frase com aspas simples
+console.log('Texto a ser exibido no terminal...');
+
+// Quarta sentença, exibindo uma frase com aspas duplas
+console.log("Utilização de aspas duplas.");
