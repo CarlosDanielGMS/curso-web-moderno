06fceba3664819dc20ffda7ecc13ef7f4ba464bf
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-01-12 14:28:19 -0300

    feat: implementa diferentes tipos de dados

---
+++ 01-fundamentos/08-dados.js
@@ -0,0 +1,9 @@
+let nome = "Ícaro";
+let salario = 75924.61;
+let idade = 25;
+let maioridade = true;
+
+console.log(typeof nome);
+console.log(typeof salario);
+console.log(typeof idade);
+console.log(typeof maioridade);
