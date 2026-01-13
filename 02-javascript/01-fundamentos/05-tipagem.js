a098c67ea1c1f1e4cf4aa0bd2cd998166f1a6468
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-01-13 10:04:31 -0300

    feat: implementa diferentes tipos de dados

---
+++ 02-javascript/01-fundamentos/05-tipagem.js
@@ -0,0 +1,8 @@
+let tipo = "Daniel"
+console.log("O valor '" + tipo + "' é do tipo " + typeof tipo);
+
+tipo = 7
+console.log("O valor '" + tipo + "' é do tipo " + typeof tipo);
+
+tipo = true
+console.log("O valor '" + tipo + "' é do tipo " + typeof tipo);
