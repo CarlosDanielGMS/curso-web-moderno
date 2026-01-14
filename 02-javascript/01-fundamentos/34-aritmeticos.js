6e2c3f3d51720ae0200a43c8068b14b869173331
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-01-14 16:05:33 -0300

    feat: implementa diferentes operadores aritméticos

---
+++ 02-javascript/01-fundamentos/34-aritmeticos.js
@@ -0,0 +1,16 @@
+const [a, b, c, d] = [7, 3, 2, 6]
+
+const soma = a + b;
+console.log('Resultado da soma: ' + soma);
+
+const subtracao = d - c;
+console.log('Resultado da subtração: ' + subtracao);
+
+const multiplicacao = b * c;
+console.log('Resultado da multiplicação: ' + multiplicacao);
+
+const divisao = d / b;
+console.log('Resultado da divisão: ' + divisao);
+
+const modulo = a % b;
+console.log('Resultado do módulo: ' + modulo);
