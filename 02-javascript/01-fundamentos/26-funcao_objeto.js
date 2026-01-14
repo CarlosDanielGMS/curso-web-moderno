1820f1fd7f5112229eda731c4e90df8e1b5df910
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-01-14 11:25:51 -0300

    feat: exibe diferentes tipos de objetos e funções

---
+++ 02-javascript/01-fundamentos/26-funcao_objeto.js
@@ -0,0 +1,10 @@
+console.log(typeof Object); // Exibe o tipo de Object
+console.log(typeof new Object); // Exibe o tipo da instância de Object
+
+const Pessoa = function() {};
+console.log(typeof Pessoa); // Exibe o tipo de Pessoa
+console.log(typeof new Pessoa); // Exibe o tipo da instância de Pessoa
+
+class Cliente {};
+console.log(typeof Cliente); // Exibe o tipo de Cliente
+console.log(typeof new Cliente); // Exibe o tipo da instância de Cliente
