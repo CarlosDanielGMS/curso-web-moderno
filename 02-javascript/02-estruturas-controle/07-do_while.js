3630324857f5a57a5901c0dd5eae230613e9fdfc
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-01-16 08:27:14 -0300

    feat: implementa laço de repetição usando do while

---
+++ 02-javascript/02-estruturas-controle/07-do_while.js
@@ -0,0 +1,22 @@
+function gerarNumeroAleatorio (minimo, maximo)
+{
+    const numero = Math.random() * (maximo - minimo) + minimo;
+    const numeroArredondado = Math.floor(numero);
+
+    return numeroArredondado;
+};
+
+let numero = 1;
+let tentativas = 0;
+
+do
+{
+    numero = gerarNumeroAleatorio(0, 100);
+    console.log('Numero gerado: ' + numero);
+
+    tentativas++;
+}
+while (numero != 0)
+
+console.log('Quantidade de tentativas: ' + tentativas);
+console.log('Fim.');
