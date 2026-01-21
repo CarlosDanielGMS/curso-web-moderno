7a4f5455958acfbde4f6e256b0751c3108511b29
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-01-21 08:58:25 -0300

    feat: implementa a resolução do exercício 10

---
+++ 02-javascript/03-funcoes/32-exercicio_10.js
@@ -0,0 +1,8 @@
+function divisivelPor3 (numero)
+{
+    return numero % 3 == 0 ? true : false;
+};
+
+console.log(`Número 3: ${divisivelPor3(3)}`);
+console.log(`Número 7: ${divisivelPor3(7)}`);
+console.log(`Número 9: ${divisivelPor3(9)}`);
