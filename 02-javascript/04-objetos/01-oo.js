dc75612a164589168ecb8674b233ba30c88c9f8f
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-01-22 14:28:02 -0300

    feat: explica conceitos importantes de oo

---
+++ 02-javascript/04-objetos/01-oo.js
@@ -0,0 +1,25 @@
+// CÓDIGO MERAMENTE EXPLICATIVO
+
+// Procedural = foco na função
+processamento(valor1, valor2, valor3);
+
+// Orientado a Objetos = foco no objeto
+objeto =
+{
+    valor1,
+    valor2,
+    valor3,
+    processamento()
+    {
+
+    }
+};
+
+objeto.processamento();
+
+// Princípios importantes da orientação a objetos
+// 1. Abstração: mapear um objeto do mundo real no mundo digital de acordo com a necessidade
+// 2. Encapsulamento: disponibilizar uma interface de interação apenas com o necessário, protegendo partes sensíveis
+// 3. Composição: possuir outros objetos (tipo de relação "tem 1"), deve ser priorizada em relação à herança
+// 4. Herança: receber atributos e funções de um objeto "pai" (tipo de relação "é 1")
+// 5. Polimorfismo: realizar a mesma ação de diferentes formas, de acordo com o objeto
