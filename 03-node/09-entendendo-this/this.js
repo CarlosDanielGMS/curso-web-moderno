70977f6bf0517d8ef7fecf827d059514f1bd6984
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-01-28 12:24:04 -0300

    feat: implementa o this em uma arrow function

--- 03-node/09-entendendo-this/this.js
+++ 03-node/09-entendendo-this/this.js
@@ -9,10 +9,22 @@ console.log(`'this' é igual a 'exports'? ${this === exports}\n`);
 function compararThis ()
 {
     console.log('Dentro de uma função...');
-    console.log(`'this' é igual a 'exports'? ${this === exports}`);
-    console.log(`'this' é igual a 'module.exports'? ${this === module.exports}`);
-    console.log(`'this' é igual a 'module'? ${this === module}`);
     console.log(`'this' é igual a 'global'? ${this === global}`);
+    console.log(`'this' é igual a 'module'? ${this === module}`);
+    console.log(`'this' é igual a 'module.exports'? ${this === module.exports}`);
+    console.log(`'this' é igual a 'exports'? ${this === exports}\n`);
 }
 
 compararThis();
+
+// Dentro uma arrow function, 'this' aponta para 'module.exports'
+const compararThisArrow = () =>
+{
+    console.log('Dentro de uma arrow function...');
+    console.log(`'this' é igual a 'global'? ${this === global}`);
+    console.log(`'this' é igual a 'module'? ${this === module}`);
+    console.log(`'this' é igual a 'module.exports'? ${this === module.exports}`);
+    console.log(`'this' é igual a 'exports'? ${this === exports}`);
+}
+
+compararThisArrow();
