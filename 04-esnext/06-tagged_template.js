8c9d1a568de385a6af210594decbcd636d719210
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-01-30 12:23:46 -0300

    feat: implementa formatação de real usando tagged templates

---
+++ 04-esnext/06-tagged_template.js
@@ -0,0 +1,23 @@
+function formatarReal (partes, ...valores)
+{
+    console.log(partes);
+    console.log(valores);
+    
+    const resultado = [];
+
+    valores.forEach
+    (
+        (valor, indice) =>
+        {
+            valor = isNaN(valor) ? valor : `R$ ${valor.toFixed(2)}`;
+            resultado.push(partes[indice], valor);
+        }
+    );
+
+    return resultado.join('');
+};
+
+const preco = 998.90;
+const precoParcela = 119.90;
+
+console.log(formatarReal `1x de ${preco} ou 10x de ${precoParcela}.`);
