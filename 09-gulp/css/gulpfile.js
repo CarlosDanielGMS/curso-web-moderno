8d1d549318f90d29f3efac32bdad8973b704da50
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-02-20 14:35:23 -0300

    feat: implementa a compilação do código css

---
+++ 09-gulp/css/gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp'); // Importa o módulo responsável pela otimização do código
+const sass = require('gulp-sass')(require('sass')); // Importa o módulo responsável pela compilação do código CSS
+const uglifyCss = require('gulp-uglifycss'); // Importa o módulo responsável por compactar/minificar o código CSS, removendo espaços em branco, comentários, quebras de linha etc
+const concat = require('gulp-concat'); // Importa o módulo responsável pela concatenação do código
+const series = gulp.series; // Importa o submódulo do Gulp responsável por executar as tarefas em série
+
+function compilarCodigo () // Instancia a função responsável por compilar o código TypeScript
+{
+    const enderecoArquivo = 'src/sass/index.scss'; // Instancia a constante com o endereço para o arquivo que importa os módulos CSS
+    const enderecoDestino = 'build/css'; // Instancia a constante com o endereço do diretório onde os arquivos transformados devem ser salvos
+    const nomeArquivoCompilado = 'estilo.min.css' // Instancia a constante com o nome do arquivo compilado a ser salvo
+    
+    return gulp.src(enderecoArquivo) // Invoca a função que seleciona os arquivos a serem utilizados no fluxo de trabalho (src), transforma eles sequencialmente (pipe) e retorna a sinalização de conclusão da tarefa
+        .pipe(sass().on('error', sass.logError)) // Invoca a função que realiza a compilação do código CSS, a que registra um ouvinte para o evento de erro e passa como parâmetro a função que exibe o erro
+        .pipe(uglifyCss({ 'uglyComments': true })) // Invoca a função que compacta/minifica o código CSS passando como parâmetro um objeto com o atributo 'uglyComments' e o valor 'true', removendo quebras de linhas e excessos de espaços dos comentários
+        .pipe(concat(nomeArquivoCompilado)) // Invoca a função que concatena os códigos passando o nome do arquivo transformado como parâmetro
+        .pipe(gulp.dest(enderecoDestino)); // Invoca a função que salva o arquivo passando o endereço de destino como parâmetro
+};
+
+exports.default = series(compilarCodigo); // Exporta a função criada como padrão e executa as funções/tarefas em série
