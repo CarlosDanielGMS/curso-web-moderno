20425a9bc6c9473bc703980987a95f74ebb31aea
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-02-20 16:51:09 -0300

    feat: implementa o processamento/copia dos arquivos

--- 09-gulp/spa/gulpfile.js
+++ 09-gulp/spa/gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp'); // Importa o módulo responsável pela otimização dos códigos
 const { series, parallel } = require('gulp'); // Importa os submódulos responsáveis por executar as tarefas em série e em paralelo
-const { processarArquivosHtml, processarArquivosCss, processarArquivosJs, processarArquivosImagens } = require('./tarefasGulp/aplicacao'); // Importa os módulos responsáveis por processar os arquivos
+const { processarArquivosHtml, processarArquivosCss, processarArquivosJs, copiarArquivosImagens } = require('./tarefasGulp/aplicacao'); // Importa os módulos responsáveis por processar e/ou copiar os arquivos
 const { importarDependenciasCss, importarDependenciasFontes } = require('./tarefasGulp/dependencias'); // Importa os módulos responsáveis por importar as dependências
 const { inicializarServidor, monitorarArquivos } = require('./tarefasGulp/servidor'); // Importa os módulos responsáveis por inicializar o servidor e monitorar os arquivos
 
@@ -8,7 +8,7 @@ module.exports.default = series // Exporta a função criada como padrão e exec
 (
     parallel // Executa as tarefas passadas como parâmetro em paralelo
     (
-        series(processarArquivosHtml, processarArquivosCss, processarArquivosJs, processarArquivosImagens), // Executa as tarefas passadas como parâmetro em série
+        series(processarArquivosHtml, processarArquivosCss, processarArquivosJs, copiarArquivosImagens), // Executa as tarefas passadas como parâmetro em série
         series(importarDependenciasCss, importarDependenciasFontes) // Paralelamente, executa as tarefas passadas como parâmetro em série
     ),
     inicializarServidor, // Posteriormente, executa a função responsável por inicializar o servidor
--- 09-gulp/spa/tarefasGulp/aplicacao.js
+++ 09-gulp/spa/tarefasGulp/aplicacao.js
@@ -1,23 +1,62 @@
 const gulp = require('gulp'); // Importa o módulo responsável pela otimização dos códigos
+const htmlMin = require('gulp-htmlmin'); // Importa o módulo responsável por compactar/minificar os códigos HTML, removendo espaços em branco, comentários, quebras de linha etc
+const sass = require('gulp-sass')(require('sass')); // Importa o módulo responsável pela compilação dos códigos CSS
+const uglifyCss = require('gulp-uglifycss'); // Importa o módulo responsável por compactar/minificar os códigos CSS, removendo espaços em branco, comentários, quebras de linha etc
+const concat = require('gulp-concat'); // Importa o módulo responsável pela concatenação dos códigos
+const babel = require('gulp-babel'); // Importa o módulo responsável por converter os códigos JS modernos em versões mais antigas (e, consequentemente, mais compatíveis com motores e navegadores desatualizados)
+const uglify = require('gulp-uglify'); // Importa o módulo responsável por compactar/minificar os códigos JS, removendo espaços em branco, comentários, quebras de linha etc
 
-function processarArquivosHtml (sinalizarConclusao) // Instancia a função responsável por processar os arquivos HTML e passa outra função como parâmetro responsável por sinalizar a conclusão da tarefa
+function processarArquivosHtml () // Instancia a função responsável por processar os arquivos HTML e passa outra função como parâmetro responsável por sinalizar a conclusão da tarefa
 {
-    return sinalizarConclusao(); // Retorna a função passada como parâmetro
+    const enderecoOrigem = 'src/**/*.html'; // Instancia a constante com o endereço para todos os arquivos com extensão '.html' presentes no diretório 'src' e em seus subdiretórios
+    const enderecoDestino = 'build'; // Instancia a constante com o endereço do diretório onde os arquivos transformados devem ser salvos
+    
+    return gulp.src(enderecoOrigem) // Invoca a função que seleciona os arquivos a serem utilizados no fluxo de trabalho (src), transforma eles sequencialmente (pipe) e retorna a sinalização de conclusão da tarefa
+        .pipe(htmlMin({ collapseWhitespace: true })) // Invoca a função que compacta/minifica os códigos HTML passando como parâmetro um objeto com o atributo 'collapseWhitespace' e o valor 'true', removendo espaços em branco
+        .pipe(gulp.dest(enderecoDestino)); // Invoca a função que salva os arquivos passando o endereço de destino como parâmetro
 };
 
-function processarArquivosCss (sinalizarConclusao) // Instancia a função responsável por processar os arquivos CSS e passa outra função como parâmetro responsável por sinalizar a conclusão da tarefa
+function processarArquivosCss () // Instancia a função responsável por processar os arquivos CSS
 {
-    return sinalizarConclusao(); // Retorna a função passada como parâmetro
+    const enderecoOrigem = 'src/assets/sass/index.scss'; // Instancia a constante com o endereço para o arquivo que importa os módulos CSS
+    const nomeArquivo = 'app.min.css' // Instancia a constante com o nome do arquivo minificado a ser salvo
+    const enderecoDestino = 'build/assets/css'; // Instancia a constante com o endereço do diretório onde os arquivos concatenados devem ser salvos
+    
+    return gulp.src(enderecoOrigem) // Invoca a função que seleciona os arquivos a serem utilizados no fluxo de trabalho (src), transforma eles sequencialmente (pipe) e retorna a sinalização de conclusão da tarefa
+        .pipe(sass().on('error', sass.logError)) // Invoca a função que realiza a compilação dos códigos CSS, a que registra um ouvinte para o evento de erro e passa como parâmetro a função que exibe o erro
+        .pipe(uglifyCss({ 'uglyComments': true })) // Invoca a função que compacta/minifica o código CSS passando como parâmetro um objeto com o atributo 'uglyComments' e o valor 'true', removendo os comentários
+        .pipe(concat(nomeArquivo)) // Invoca a função que concatena os códigos passando o nome do arquivo minificado como parâmetro
+        .pipe(gulp.dest(enderecoDestino)); // Invoca a função que salva os arquivos passando o endereço de destino como parâmetro
 };
 
-function processarArquivosJs (sinalizarConclusao) // Instancia a função responsável por processar os arquivos JS e passa outra função como parâmetro responsável por sinalizar a conclusão da tarefa
+function processarArquivosJs () // Instancia a função responsável por processar os arquivos JS
 {
-    return sinalizarConclusao(); // Retorna a função passada como parâmetro
+    const enderecoOrigem = 'src/assets/js/**/*.js'; // Instancia a constante com o endereço para todos os arquivos com extensão '.js' presentes no diretório 'src/assets/js' e em seus subdiretórios
+    const nomeArquivo = 'app.min.js' // Instancia a constante com o nome do arquivo minificado a ser salvo
+    const enderecoDestino = 'build/assets/js'; // Instancia a constante com o endereço do diretório onde os arquivos concatenados devem ser salvos
+    
+    return gulp.src(enderecoOrigem) // Invoca a função que seleciona os arquivos a serem utilizados no fluxo de trabalho (src), transforma eles sequencialmente (pipe) e retorna a sinalização de conclusão da tarefa
+        .pipe // Invoca a função que adiciona uma função à "esteira" de execuções
+        (
+            babel // Invoca a função que realiza a conversão dos códigos JS modernos em versões mais antigas
+            (
+                { // Passa um objeto de opções como parâmetro da função
+                    presets: ['env'], // Define o valor do atributo 'presets' do objeto como '['env']', considerando a versão mais atualizada do JavaScript como base
+                }
+            )
+        )
+        .pipe(uglify()) // Invoca a função que compacta/minifica os códigos JS, removendo espaços em branco, comentários, quebras de linha etc
+        .pipe(concat(nomeArquivo)) // Invoca a função que concatena os códigos passando o nome do arquivo minificado como parâmetro
+        .pipe(gulp.dest(enderecoDestino)); // Invoca a função que salva os arquivos passando o endereço de destino como parâmetro
 };
 
-function processarArquivosImagens (sinalizarConclusao) // Instancia a função responsável por processar os arquivos de imagens e passa outra função como parâmetro responsável por sinalizar a conclusão da tarefa
+function copiarArquivosImagens () // Instancia a função responsável por copiar os arquivos de imagens para a pasta de construção
 {
-    return sinalizarConclusao(); // Retorna a função passada como parâmetro
+    const enderecoOrigem = 'src/assets/imgs/**/*.*'; // Instancia a constante com o endereço para todos os arquivos presentes no diretório 'src/assets/imgs' e em seus subdiretórios
+    const enderecoDestino = 'build/assets/imgs'; // Instancia a constante com o endereço do diretório onde o arquivo HTML deve ser salvo
+    
+    return gulp.src(enderecoOrigem) // Invoca a função que seleciona os arquivos a serem utilizados no fluxo de trabalho e retorna a sinalização de conclusão da tarefa
+        .pipe(gulp.dest(enderecoDestino)); // Invoca a função que salva os arquivos do fluxo de trabalho no endereço de destino especificado nos parâmetros da função
 };
 
 module.exports = // Exporta os módulos passados como parâmetro
@@ -25,5 +64,5 @@ module.exports = // Exporta os módulos passados como parâmetro
     processarArquivosHtml,
     processarArquivosCss,
     processarArquivosJs,
-    processarArquivosImagens
+    copiarArquivosImagens
 };
