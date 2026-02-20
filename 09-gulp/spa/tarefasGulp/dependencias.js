42a7d4f8f9612b1e04a3e88c7d409f2114e0582b
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-02-20 16:06:59 -0300

    feat: implementa a importação das dependências

--- 09-gulp/spa/tarefasGulp/dependencias.js
+++ 09-gulp/spa/tarefasGulp/dependencias.js
@@ -1,13 +1,26 @@
 const gulp = require('gulp'); // Importa o módulo responsável pela otimização dos códigos
+const uglifyCss = require('gulp-uglifycss'); // Importa o módulo responsável por compactar/minificar o código CSS, removendo espaços em branco, comentários, quebras de linha etc
+const concat = require('gulp-concat'); // Importa o módulo responsável pela concatenação dos códigos
 
 function importarDependenciasCss (sinalizarConclusao) // Instancia a função responsável por importar as dependências CSS e passa outra função como parâmetro responsável por sinalizar a conclusão da tarefa
 {
-    return sinalizarConclusao(); // Retorna a função passada como parâmetro
+    const enderecoOrigem = 'node_modules/font-awesome/css/font-awesome.css'; // Instancia a constante com o endereço para o arquivo CSS
+    const nomeArquivoMinificado = 'deps.min.css' // Instancia a constante com o nome do arquivo minificado a ser salvo
+    const enderecoDestino = 'build/assets/css'; // Instancia a constante com o endereço do diretório onde o arquivo concatenado deve ser salvo
+    
+    return gulp.src(enderecoOrigem) // Invoca a função que seleciona o arquivo a ser utilizado no fluxo de trabalho (src), transforma ele sequencialmente (pipe) e retorna a sinalização de conclusão da tarefa
+        .pipe(uglifyCss({ 'uglyComments': false })) // Invoca a função que compacta/minifica o código CSS passando como parâmetro um objeto com o atributo 'uglyComments' e o valor 'false', evitando a remoção de quebras de linhas e excessos de espaços dos comentários
+        .pipe(concat(nomeArquivoMinificado)) // Invoca a função que concatena os códigos passando o nome do arquivo minificado como parâmetro
+        .pipe(gulp.dest(enderecoDestino)); // Invoca a função que salva o arquivo passando o endereço de destino como parâmetro
 };
 
 function importarDependenciasFontes (sinalizarConclusao) // Instancia a função responsável por importar as dependências de fontes e passa outra função como parâmetro responsável por sinalizar a conclusão da tarefa
 {
-    return sinalizarConclusao(); // Retorna a função passada como parâmetro
+    const enderecoOrigem = 'node_modules/font-awesome/fonts/*.*'; // Instancia a constante com o endereço para os arquivos de fonte
+    const enderecoDestino = 'build/assets/fonts'; // Instancia a constante com o endereço do diretório onde os arquivos devem ser salvos
+    
+    return gulp.src(enderecoOrigem) // Invoca a função que seleciona os arquivos a serem utilizados no fluxo de trabalho (src), transforma eles sequencialmente (pipe) e retorna a sinalização de conclusão da tarefa
+        .pipe(gulp.dest(enderecoDestino)); // Invoca a função que salva os arquivos passando o endereço de destino como parâmetro
 };
 
 module.exports =  // Exporta os módulos passados como parâmetro
