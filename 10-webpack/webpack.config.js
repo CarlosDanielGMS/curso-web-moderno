5dedcc3617580d61dcbaad96b8b802d7b47508b2
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-02-23 16:09:06 -0300

    feat: externaliza arquivos css

--- 10-webpack/webpack.config.js
+++ 10-webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack'); // Importa o módulo responsável por compilar módulos JavaScript
+const miniCssExtractPlugin = require('mini-css-extract-plugin'); // Importa o módulo responsável por externalizar os módulos CSS
 
 module.exports = // Exporta o módulo de configuração do webpack
 {
@@ -10,6 +11,15 @@ module.exports = // Exporta o módulo de configuração do webpack
         path: __dirname + '/public', // Configura o diretório de saída do webpack
         filename: 'principal.js' // Configura o nome do arquivo de saída do webpack
     },
+    plugins: // Configura a lista de extensões do webpack
+    [
+        new miniCssExtractPlugin // Instancia o módulo responsável por externalizar os módulos CSS
+        (
+            {
+                filename: 'estilo.css' // Configura o nome do arquivo CSS a ser externalizado pelo módulo
+            }
+        )
+    ],
     module: // Configura como o webpack deve lidar com os módulos/arquivos
     {
         rules: // Configura a lista de regras do webpack
@@ -18,7 +28,8 @@ module.exports = // Exporta o módulo de configuração do webpack
                 test: /\.css$/, // Indentifica o tipo de arquivo a utilizar os carregadores
                 use: // Configura a lista de carregadores
                 [
-                    'style-loader', // Adiciona o CSS na página injetando um elemento <style> no DOM para cada arquivo CSS
+                    miniCssExtractPlugin.loader, // Externaliza os arquivos CSS
+                    // 'style-loader', // Adiciona o CSS na página injetando um elemento <style> no DOM para cada arquivo CSS
                     'css-loader' // Interpreta os comandos @import e url() dentro do arquivo CSS
                 ]
             }
