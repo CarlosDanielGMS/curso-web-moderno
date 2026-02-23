a2bdd051fb6bc0e3d216c94b662845d0a2c6255c
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-02-23 15:21:27 -0300

    feat: configura a saída do webpack

--- 10-webpack/webpack.config.js
+++ 10-webpack/webpack.config.js
@@ -4,5 +4,10 @@ module.exports = // Exporta o módulo de configuração do webpack
 {
     // mode: 'production', // Configura o webpack para o modo produção
     mode: 'development', // Configura o webpack para o modo desenvolvimento
-    entry: './src/principal.js' // Configura o arquivo de entrada do webpack
+    entry: './src/principal.js', // Configura o arquivo de entrada do webpack
+    output: // Configura a saída do webpack
+    {
+        path: __dirname + '/public', // Configura o diretório de saída do webpack
+        filename: 'principal.js' // Configura o nome do arquivo de saída do webpack
+    }
 };
