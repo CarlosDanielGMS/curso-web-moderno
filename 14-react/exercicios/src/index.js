ebdabc72462e4a731ea19e367ce67c083a2b5c08
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-03-05 08:46:53 -0300

    feat: cria o primeiro componente react

--- 14-react/exercicios/src/index.js
+++ 14-react/exercicios/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'; // Importa a biblioteca principal do React
 import ReactDOM from 'react-dom/client'; // Importa a nova biblioteca de DOM do React
 
-// Cria o elemento H1 do React usando a sintaxe JSX (que será convertido em 'React.createElement('h1', null, 'React');')
-const elementoH1 = <h1>React</h1>;
+// Importa o componente React
+import PrimeiroComponente from './componentes/primeiroComponente';
 
 // Cria a raiz do React apontando para o elemento do HTML com ID 'root'
 const raiz = ReactDOM.createRoot(document.getElementById('root'));
 
-// Monta e renderiza o elemento H1 na árvore do DOM do navegador
-raiz.render(elementoH1);
+// Monta e renderiza o componente React na árvore do DOM do navegador
+raiz.render(<PrimeiroComponente/>);
