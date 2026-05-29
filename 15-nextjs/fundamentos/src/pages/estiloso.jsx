13e671044d9f54847a73498599e700ab13229e99
Carlos Daniel <carlos.daniel@medeirosgalvao.com.br>
2026-05-29 10:37:38 -0300

    feat: implementa módulos css

---
+++ 15-nextjs/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,10 @@
+import styles from "./Estiloso.module.css";
+
+export default function Estiloso ()
+{
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando módulos CSS</h1>
+        </div>
+    );
+};
